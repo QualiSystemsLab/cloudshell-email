7aaf6d3b82521ff3614547314228b800e36d60c8
DYeag <37943859+DYeag@users.noreply.github.com>
2020-11-03 11:23:44 -0800

    update filename
    change file anme so it is different from directory name

--- package/cloudshell/email/__init__.py
+++ package/cloudshell/email/__init__.py
@@ -2,5 +2,5 @@ from pkgutil import extend_path
 
 __path__ = extend_path(__path__, __name__)
 
-from .email import EmailService
+from .email_service import EmailService
 from .email_config import EmailConfig
