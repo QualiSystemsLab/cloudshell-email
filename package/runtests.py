5c1d78bbd8ac96ec2967d37c396f761e2b63ed3e
alexazarh <aazarh@gmail.com>
2020-11-10 16:03:59 -0600

    try to fix ci

--- package/runtests.py
+++
@@ -1,9 +0,0 @@
-import nose
-import nose.config
-import sys
-from nose.plugins.manager import DefaultPluginManager
-c = nose.config.Config()
-c.plugins=DefaultPluginManager()
-c.srcDirs = ['test']
-if not nose.run(config=c):
-    sys.exit(1)
